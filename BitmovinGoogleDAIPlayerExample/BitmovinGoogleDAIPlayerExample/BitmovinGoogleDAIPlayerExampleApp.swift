fcb9674e0087a0a6902b337443085dac809f3617
David Steinacher <david.steinacher@bitmovin.com>
2026-07-06 16:37:09 +0200

    set up initial Swift package

---
+++ BitmovinGoogleDAIPlayerExample/BitmovinGoogleDAIPlayerExample/BitmovinGoogleDAIPlayerExampleApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct BitmovinGoogleDAIPlayerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Package.swift
@@ -0,0 +1,45 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BitmovinGoogleDAIPlayer",
+    platforms: [
+        .iOS(.v15),
+        .tvOS(.v15)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "BitmovinGoogleDAIPlayer",
+            targets: ["BitmovinGoogleDAIPlayer"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/bitmovin/player-ios-core.git", from: "3.116.1"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-interactive-media-ads-ios", exact: "3.31.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-interactive-media-ads-tvos", exact: "4.16.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "BitmovinGoogleDAIPlayer",
+            dependencies: [
+                .product(name: "BitmovinPlayerCore", package: "player-ios-core"),
+                .product(
+                    name: "GoogleInteractiveMediaAds",
+                    package: "swift-package-manager-google-interactive-media-ads-ios",
+                    condition: .when(platforms: [.iOS])
+                ),
+                .product(
+                    name: "GoogleInteractiveMediaAdsTvOS",
+                    package: "swift-package-manager-google-interactive-media-ads-tvos",
+                    condition: .when(platforms: [.tvOS])
+                )
+            ],
+            path: "BitmovinGoogleDAIPlayer"
+        ),
+    ]
+)
