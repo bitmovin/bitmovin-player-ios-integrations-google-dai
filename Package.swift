8b503d8a13cb9314b2a1d95363feb6d50dd038e7
David Steinacher <david.steinacher@bitmovin.com>
2026-07-14 15:50:33 +0200

    depend on umbrella player package

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/bitmovin/player-ios-core.git", from: "3.116.1"),
+        .package(url: "https://github.com/bitmovin/player-ios.git", exact: "3.118.0-a.1"),
         .package(url: "https://github.com/googleads/swift-package-manager-google-interactive-media-ads-ios", exact: "3.31.0"),
         .package(url: "https://github.com/googleads/swift-package-manager-google-interactive-media-ads-tvos", exact: "4.16.0")
     ],
@@ -27,7 +27,7 @@ let package = Package(
         .target(
             name: "BitmovinGoogleDAIPlayer",
             dependencies: [
-                .product(name: "BitmovinPlayerCore", package: "player-ios-core"),
+                .product(name: "BitmovinPlayer", package: "player-ios"),
                 .product(
                     name: "GoogleInteractiveMediaAds",
                     package: "swift-package-manager-google-interactive-media-ads-ios",
