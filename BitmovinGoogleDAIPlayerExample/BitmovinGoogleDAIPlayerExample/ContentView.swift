71358ebdc504bd31ddb8b0fa96f83141c0d5cee9
David Steinacher <david.steinacher@bitmovin.com>
2026-07-14 15:51:27 +0200

    demonstrate Google DAI API in example app

--- BitmovinGoogleDAIPlayerExample/BitmovinGoogleDAIPlayerExample/ContentView.swift
+++ BitmovinGoogleDAIPlayerExample/BitmovinGoogleDAIPlayerExample/ContentView.swift
@@ -1,22 +1,41 @@
 import BitmovinGoogleDAIPlayer
+import BitmovinPlayerCore
+import Foundation
 import SwiftUI
 
 struct ContentView: View {
+    private let player: Player
+
+    init() {
+        let config = PlayerConfig()
+        config.key = "YOUR-LICENSE-KEY"
+
+        player = PlayerFactory.createGoogleDaiPlayer(
+            playerConfig: config
+        )
+    }
+
     var body: some View {
         VStack(spacing: 20) {
-            Spacer()
-
             Text("Bitmovin Google DAI Player")
                 .font(.largeTitle)
                 .bold()
                 .multilineTextAlignment(.center)
 
-            Text("Demo app setup is ready. Integration implementation is intentionally empty.")
-                .multilineTextAlignment(.center)
+            VideoPlayerView(
+                player: player
+            )
+            .aspectRatio(16 / 9, contentMode: .fit)
+            .cornerRadius(25)
 
             Spacer()
         }
         .padding()
+        .onAppear {
+            let googleDaiSource = GoogleDaiSource()
+
+            player.googleDai.load(source: googleDaiSource)
+        }
     }
 }
 
